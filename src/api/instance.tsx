a52518349b12eea4f2235703d401bb6f4192e7a1
caotruong0511 <truongcmph15429@fpt.edu.vn>
2022-03-23 00:36:10 +0700

    router

--- src/api/instance.tsx
+++ src/api/instance.tsx
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
+
 const instance = axios.create({
-    baseURL:"http://localhost:3001/api"
-})
+    baseURL: "http://localhost:3001/api"
+});
 export default instance;
