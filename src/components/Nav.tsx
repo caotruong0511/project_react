90e4f78005d6c4f8b8045179b47ed68f55dd1527
caotruong0511 <truongcmph15429@fpt.edu.vn>
2022-03-28 23:08:11 +0700

    nav:category

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,34 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { list } from '../api/category'
+import { Category } from '../types/category'
 
 type Props = {}
 
 const Nav = (props: Props) => {
+    const [cate,setcate]=useState<Category[]>([])
+    useEffect(()=>{
+        const cate=async()=>{
+        const {data}=await list()
+        setcate(data)
+        }
+        cate()
+    },[])
   return (
-    <div>
-  
-  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/product">Product</NavLink>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
+      <div>
+          <ul>
+                <li className="inline"><NavLink className="px-3 text-lg font-semibold" to="/">Home Page</NavLink></li>
+                <div className='relative inline'>
+                <li id="categories" className="inline"><a href="/" className="px-3 text-lg font-semibold py-16">Danh mục</a></li>
+                <ul className="sub_menu absolute left-0 z-50 bg-white">
+                    {cate?.map((e,index) => {
+                        return(
+                            <li key={index} className="rounded-md"><a href={`/category/${e._id}`} className="pr-24 py-3 hover:bg-red-500 px-10 py-1 block text-lg font-semibold">{e.name}</a></li>
+                        )
+                    })}
+                </ul>
+                </div>
+                <li className="inline"><NavLink className="px-3 text-lg font-semibold" to="/product">product</NavLink></li>
+                <li className="inline"><a href="/order_manage" className="px-3 text-lg font-semibold">Đơn hàng</a></li>
+                <li className="inline"><a href="/" className="px-3 text-lg font-semibold">Bảo hành</a></li>
+            </ul>
     </div>
   )
 }
